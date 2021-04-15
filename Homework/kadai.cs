2145b69557e35937091de3815302161fa646bc9b
tmtmNasubi <59120909+tmtmNasubi@users.noreply.github.com>
2021-04-16 00:00:37 +0900

    C#

---
+++ Homework/kadai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace kadai1
+{
+    class kadai
+    {
+        static void Main(string[] args)
+        {
+            int sum, add;
+            sum = 0;
+            add = 0;
+            int max = int.Parse(Console.ReadLine());
+
+            while(max > sum)
+            {
+                sum = sum + add;
+
+                if (max > sum)
+                {
+                    add = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            Console.WriteLine("すべてのaddの合計値(< max)は{0}です。", sum);
+        }
+    }
+}
